2cc9f0e372f20a2a021e98ca8b94c7adf1efc299
Kevin Montag <kmontag@cs.stanford.edu>
2019-06-25 02:09:40 +0200

    Remove commented stuff

--- kontrol/featherstreamer/src/index.ts
+++ kontrol/featherstreamer/src/index.ts
@@ -12,37 +12,3 @@ const port = 44668;
 app.listen(port, '0.0.0.0', () => {
   console.log(`Server listening on port ${port}.`);
 });
-
-// import * as dgram from 'dgram';
-// import * as os from 'os';
-
-// const port = 44668;
-// const multicastAddr = '224.0.0.7';
-
-// const socket = dgram.createSocket({
-//   type: 'udp4',
-//   reuseAddr: true
-// });
-// socket.bind(port);
-// socket.on('listening', () => {
-//   socket.addMembership(multicastAddr);
-
-//   let numSent: number = 0;
-
-//   setInterval(() => {
-//     numSent++;
-//     const message = `Hello from ${os.hostname} (${numSent})`;
-//     socket.send(message, 0, message.length, port, multicastAddr, () => {
-//       console.log(`SENDING: ${message}`);
-//     });
-//   }, 5000);
-
-
-//   const address = socket.address();
-//   const addressStr: string = (typeof address === 'string') ? address : `${address.address}:${address.port}`;
-//   console.log(`UDP socket listening on ${addressStr}.`);
-// });
-
-// socket.on('message', (message, rinfo) => {
-//   console.log(`Message from ${rinfo.address}:${rinfo.port}: ${message}`);
-// });
