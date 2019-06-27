5ed2c78be235e4bd5f7881fd408334caea7aa234
Kevin Montag <kmontag@cs.stanford.edu>
2019-06-27 12:20:47 +0200

    UI tweaks, set up functional web server on device

--- kontrol/featherstreamer-web/src/components/RotaryEncoderControls/index.tsx
+++ kontrol/featherstreamer-web/src/components/RotaryEncoderControls/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Grid, Button, ButtonProps, Icon } from 'semantic-ui-react';
+import { Grid, Button, ButtonProps } from 'semantic-ui-react';
 import style from './style.module.scss';
 import { ApplicationState, ConnectedReduxProps } from '../../store';
 import { RotaryEncoderState } from '../../store/rotaryEncoder/reducer';
 import { press, release, rotateClockwise, rotateCounterclockwise } from '../../store/rotaryEncoder/actions';
+import knob from './knob.svg';
 
 type Props = RotaryEncoderState & ConnectedReduxProps;
 
@@ -28,45 +29,54 @@ class RotaryEncoderControls extends React.Component<Props> {
     const pushProps: ButtonProps = Object.assign({}, buttonProps, {
       color: isPressed ? 'pink' : 'blue',
     });
+    const knobProps: ButtonProps = Object.assign({}, buttonProps, {
+      color: 'white',
+      inverted: isPressed,
+    });
 
     return (
       <div className={style.root}>
         <Grid relaxed>
-          <Grid.Column width={14}>
-            <Grid columns={3} relaxed>
-              <Grid.Column>
-                <Button {...buttonProps}
-                  onClick={() => dispatch(rotateCounterclockwise())}
-                >
-                  LEFT
-                </Button>
-              </Grid.Column>
-              <Grid.Column>
-                <Button {...buttonProps}
-                  onClick={() => dispatch(rotateClockwise())}
-                >
-                  RIGHT
-                </Button>
-              </Grid.Column>
-              <Grid.Column>
+          <Grid.Column width={5}>
+            <Button {...buttonProps}
+              onClick={() => dispatch(rotateCounterclockwise())}
+            >
+              LEFT
+            </Button>
+          </Grid.Column>
+          <Grid.Column width={5}>
+            <Button {...buttonProps}
+              onClick={() => dispatch(rotateClockwise())}
+            >
+              RIGHT
+            </Button>
+          </Grid.Column>
+          <Grid.Column width={6}>
+            <Grid>
+              <Grid.Column width={11}>
                 <Button {...pushProps}
                   onMouseDown={() => dispatch(press())}
                   onMouseUp={() => dispatch(release())}>
                   { isPressed ? 'RELEASE' : 'PUSH' }
                 </Button>
               </Grid.Column>
+              <Grid.Column width={5}>
+                <Button {...knobProps}
+                  onMouseDown={() => dispatch(press())}
+                  onMouseUp={() => dispatch(release())}>
+                  <div className={style.dialWrapper}>
+                    <svg className={[
+                      style.dial,
+                      isPressed ? style.active : style.inactive,
+                      style[`rotate-${position}`],
+                    ].join(' ')} height="100%" width="100%">
+                      <image xlinkHref={knob} height="100%" width="100%" />
+                    </svg>
+                    <span>&nbsp;</span>
+                  </div>
+                </Button>
+              </Grid.Column>
             </Grid>
-          </Grid.Column>
-          <Grid.Column width={2}>
-            <div className={[
-              style.dial,
-              isPressed ? style.active : style.inactive,
-              style[`rotate-${position}`],
-            ].join(' ')}>
-                <Icon name="circle notch" className={[
-                  style.icon,
-                ].join(' ')} />
-              </div>
           </Grid.Column>
         </Grid>
       </div>
